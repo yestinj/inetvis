2c1a0a2f906a79352cb5b426f6b8b786a9f892f2
Yestin <yestinj@gmail.com>
2017-04-02 19:05:09 +0200

    Added the inners of the reference frame setting wrapper.

--- src/referenceframesettingsdialogwidget.cpp
+++ src/referenceframesettingsdialogwidget.cpp
@@ -1,6 +1,57 @@
 #include "referenceframesettingsdialogwidget.h"
+#include <iostream>
 
 ReferenceFrameSettingsDialogWidget::ReferenceFrameSettingsDialogWidget(QWidget *parent) :
     QDialog(parent) {
     setupUi(this);
+}
+
+void ReferenceFrameSettingsDialogWidget::applyReferenceFrame() {
+    emit setReferenceFrame(primaryAxesCheckBox->isChecked(),
+                           boundingAxesCheckBox->isChecked(),
+                           cubeCheckBox->isChecked(),
+                           planeCheckBox->isChecked(),
+                           markersCheckBox->isChecked());
+}
+
+
+void ReferenceFrameSettingsDialogWidget::applyGrid() {
+    emit setGridLines(frontCheckBox->isChecked(),
+                      backCheckBox->isChecked(),
+                      leftCheckBox->isChecked(),
+                      rightCheckBox->isChecked(),
+                      topCheckBox->isChecked(),
+                      bottomCheckBox->isChecked(),
+                      planeGridCheckBox->isChecked(),
+                      (opacitySpinBox->value() / 100.0));
+    //the tranparency value is accepted as a fractiuon, and is in fact
+    //the opacity value i.e. 100 would be fully opague, 0 fully transparent
+}
+
+
+void ReferenceFrameSettingsDialogWidget::applyPartitions() {
+    emit setPartitions(xAxisSpinBox->value(), yAxisSpinBox->value(), zAxisSpinBox->value());
+}
+
+
+void ReferenceFrameSettingsDialogWidget::axisLabelsStateChange(bool axesOn) {
+    emit displayAxisLabels(axesOn);
+}
+
+
+void ReferenceFrameSettingsDialogWidget::dateTimeLabelStateChange(bool timeOn) {
+    emit displayDateTime(timeOn);
+}
+
+
+void ReferenceFrameSettingsDialogWidget::framerateLabelStateChange(bool fpsOn) {
+    emit displayFramerate(fpsOn);
+}
+
+
+void ReferenceFrameSettingsDialogWidget::projectionModeChanged() {
+    if (perspectiveRadioButton->isChecked())
+        emit setPerspectiveProjection();
+    else
+        emit setOrthographicProjection();
 }
--- src/referenceframesettingsdialogwidget.h
+++ src/referenceframesettingsdialogwidget.h
@@ -3,6 +3,7 @@
 
 #include <QDialog>
 #include "ui_referenceframesettingsdialog.h"
+#include "logui.h"
 
 class ReferenceFrameSettingsDialogWidget : public QDialog, public Ui::ReferenceFrameSettingsDialog {
     Q_OBJECT
@@ -10,8 +11,23 @@ public:
     ReferenceFrameSettingsDialogWidget(QWidget *parent = 0);
 
 signals:
+    void setGridLines(bool, bool, bool, bool, bool, bool, bool, float);
+    void setReferenceFrame(bool, bool, bool, bool, bool);
+    void setPartitions(int, int, int);
+    void setPerspectiveProjection();
+    void setOrthographicProjection();
+    void displayAxisLabels(bool);
+    void displayDateTime(bool);
+    void displayFramerate(bool);
 
 public slots:
+    void applyReferenceFrame();
+    void applyGrid();
+    void applyPartitions();
+    void axisLabelsStateChange(bool);
+    void dateTimeLabelStateChange(bool);
+    void framerateLabelStateChange(bool);
+    void projectionModeChanged();
 
 private:
 
